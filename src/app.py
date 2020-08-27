111ec6dd935a2235473cd6900ce15fa7bce129f1
Eduardo Calderon <educr6@gmail.com>
2020-08-27 11:17:48 -0400

    initial structure

--- src/app.py
+++ src/app.py
@@ -1,14 +1,30 @@
-from flask import Flask 
+from flask import Flask
+from flask import request
+from .brokerx_client import get_balance_of_account
 
 def create_app():
     app = Flask(__name__)
 
     app.config.from_pyfile('settings.py')
 
+   
+    
     @app.route('/')
     def index():
+        return "Hello World"
+    
 
-        return app.config.get('API_KEY')
+    @app.route('/getaccountbalance')
+    def get_accout_balance():
+
+        alias = request.args.get('alias')
+        monto = get_balance_of_account(2, alias)
+
+        return {
+            "status": "OK",
+            "message": "El balance en su cuenta es de %s pesos" % monto,
+            "operation success": True
+        }
     
     return app
 
