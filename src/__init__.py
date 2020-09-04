7b73818febfe1b808a2958b2e13a31768066b99b
Eduardo Calderon <educr6@gmail.com>
2020-09-04 18:37:05 -0400

    added available credit endpoint functionality

--- src/__init__.py
+++ src/__init__.py
@@ -45,4 +45,17 @@ def create_app():
             "message": "El limite de su tarjeta de crédito %s es de %s pesos" % (alias, limit),
             "operation success": True}
     
+
+    @app.route('/getcreditcardavailablecredit')
+    def get_credit_card_available_credit():
+
+        alias = request.args.get('alias')
+        corex_client = CoreXClient(app.config['COREX_BASE_URL'], 2)
+        available_credit = corex_client.get_credit_card_available_credit(alias)
+
+        return {
+            "status": "OK",
+            "message": "El credito disponible de su tarjeta de crédito %s es de %s pesos" % (alias, available_credit),
+            "operation success": True}
+    
     return app
--- src/corex_client.py
+++ src/corex_client.py
@@ -39,6 +39,19 @@ class CoreXClient:
 
         return credit_card_data['limiteCredito']
 
+    
+    def get_credit_card_available_credit(self, alias):
+
+        accounts = self.get_credit_cards_from_client()
+
+        if (self.account_exists(accounts, alias) == False):
+            return None
+        
+        product = self.select_product(accounts, alias)
+        credit_card_data = self.get_credit_card_data(product)
+
+        return credit_card_data['balance']
+
 
 
     def get_credit_card_data(self, product):
