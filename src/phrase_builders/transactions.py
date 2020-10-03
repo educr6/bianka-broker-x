3ce6064175ff341f20a2e2d3c266d7e7f8ae457d
Eduardo Calderon <educr6@gmail.com>
2020-10-03 13:02:42 -0400

    added function thats read the date from a transaction and returns it in a verbal way + some unit tests for it

--- src/phrase_builders/transactions.py
+++ src/phrase_builders/transactions.py
@@ -1,8 +1,11 @@
-
-
+from datetime import datetime
 #def create_transaction_phrase(transactions, number_of_transactions):
  #   return
    # selected_transactions = transactions[:]
+
+months_in_spanish = {1: "enero", 2: "febrero", 3: "marzo", 
+4: "abril", 5: "mayo", 6: "junio", 7: "julio", 8:"agosto",
+ 9:"septiembre", 10:"octubre", 11:"noviembre", 12:"diciembre"}
 
 
 def trim_transactions(transaction_list, number_of_transactions):
@@ -13,8 +16,14 @@ def create_phrase_for_single_transaction(transaction):
     phrase = "%s, en la fecha %s, por un monto de %s pesos" % ("uan", "two", "three")
     return phrase
 
-def read_date_from_transaction(transaction):
-    return
+def get_date_from_transaction_in_phrase(transaction):
+
+    date_string = transaction["transactionDate"].split('T')[0]
+    dt_object = datetime.strptime(date_string, '%Y-%m-%d')
+
+    date_phrase = "%s de %s" % (dt_object.day, months_in_spanish[dt_object.month])
+
+    return date_phrase
 
 
 
--- tests/test_transactions_phrase_builders.py
+++ tests/test_transactions_phrase_builders.py
@@ -18,3 +18,24 @@ def test_trim_transactions2():
     trimmed_transactions = phrase_builders.trim_transactions(mock_transactions, 2)
 
     assert expected == len(trimmed_transactions)
+
+def test_get_date_from_transaction_in_phrase1():
+
+    mock_transaction = {"transactionDate": '2020-09-18T20:52:24.897'}
+    expected = '18 de septiembre'
+
+    assert expected == phrase_builders.get_date_from_transaction_in_phrase(mock_transaction)
+
+def test_get_date_from_transaction_in_phrase2():
+
+    mock_transaction = {"transactionDate": '2020-07-12T20:52:24.897'}
+    expected = '12 de julio'
+
+    assert expected == phrase_builders.get_date_from_transaction_in_phrase(mock_transaction)
+
+def test_get_date_from_transaction_in_phrase3():
+
+    mock_transaction = {"transactionDate": '2020-01-01T20:52:24.897'}
+    expected = '1 de enero'
+
+    assert expected == phrase_builders.get_date_from_transaction_in_phrase(mock_transaction)
